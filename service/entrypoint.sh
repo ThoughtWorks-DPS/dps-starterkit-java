3a4bcd476f40ab9a4c6fae64ba597dc2083d6c7e
Ranbir Chawla <rchawla@thoughtworks.com>
2020-12-16 11:09:35 -0700

    Using alternate method for building docker for more consistency of startup

--- service/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# prepare the full URL based on the hostname from AWS
-
-export SPRING_APPLICATION_DATASOURCE_URL="postgres://$POSTGRES_HOST:5432/$POSTGRES_DBNAME"
-
-java -jar /starer-java.jar
