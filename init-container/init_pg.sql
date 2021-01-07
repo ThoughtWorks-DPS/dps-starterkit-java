f23c6a1ca8cef88a3eefc4cb77336d9f95f23c8f
Ranbir Chawla <rchawla@thoughtworks.com>
2021-01-07 14:25:56 -0700

    fixing bug in psql script and updating deployment files

--- init-container/init_pg.sql
+++ init-container/init_pg.sql
@@ -1,7 +1,7 @@
 -- "username" and "dbname" are variables that must be
 -- passed in from the commandline
 -- ex) psql "$POSTGRES_URL" -v username="$USERNAME" -v dbname="$DBNAME" -f init_pg.sql
-CREATE USER :username  WITH CREATEDB, CREATEROLE;
+CREATE USER :username  WITH CREATEDB CREATEROLE;
 ALTER ROLE :username login password ':password';
 CREATE DATABASE :username;
 CREATE DATABASE :dbname;
