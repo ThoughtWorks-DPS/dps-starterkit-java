25c6510de6fef8d54a0fa80dfe9a0fc6a1c165ce
Carl Nygard <carl.nygard@thoughtworks.com>
2021-01-21 00:54:45 -0500

    add gatling scala code to exercise service during load/perf testing
    - added short scala test code to drive the /health and /starter API endpoints

---
+++ app/src/gatling/scala/io/twdps/starter/api/StarterApiSimulation.scala
@@ -0,0 +1,28 @@
+package io.twdps.starter.api
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration.DurationInt
+
+class StarterApiSimulation extends Simulation {
+
+  val httpProtocol = http
+    .warmUp("http://localhost:8080/health")
+    .baseUrl("http://localhost:8080") // Here is the root for all relative URLs
+    .acceptHeader("application/json") // Here are the common headers
+
+  val scn = scenario("API Endpoint Simulation")
+    .exec(http("Create apple")
+      .post("/starter")
+      .body(StringBody("""{"userName":"mary.q.contrary", "firstName": "Mary", "lastName":"Contrary"}"""))
+      .asJson)
+    .exec(http("Get account")
+      .get("/starter/1"))
+
+  setUp(scn
+    .inject(
+      atOnceUsers(10),
+      rampUsersPerSec(10) to 100 during (2 minutes))
+    .protocols(httpProtocol))
+}
